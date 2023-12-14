af08016d5406bf038110982e5c04ac6b0a5d085a
DiegoJGlez <diejovigo83@gmail.com>
2023-12-14 17:31:42 +0100

    1º commit

--- src/modelo/FiguraGeometrica.java
+++ src/modelo/FiguraGeometrica.java
@@ -2,9 +2,35 @@ package modelo;
 
 public class FiguraGeometrica {
 
+// Diego Jose Gonzalez Cobelas
+
     private int numLados;
     private String nombre;
     private static final int MIN_NUMBER = 3;
+
+
+    public int getNumLados() {
+        return this.numLados;
+    }
+
+    public void setNumLados(int numLados) {
+        this.numLados = numLados;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public FiguraGeometrica() {
+    }
+
+
+
 
     public void showInfo() {
         System.out.printf("Soy un %s. Tengo %d lados%n", nombre, numLados);
